603f80395ea6e7fad261b288ba266324f099ecec
Franco <89819221+FrancoDomingo@users.noreply.github.com>
2023-03-21 17:15:16 -0300

    pagination added

--- client/src/components/HistoryList.js
+++ client/src/components/HistoryList.js
@@ -1,11 +1,21 @@
 import {useEffect, useState} from 'react'
 import {useNavigate} from 'react-router-dom'
-import {Button, Card, CardContent, CardMedia, CardActions, Typography, Grid, Skeleton, Stack, Pagination} from '@mui/material'
+import {Button, Card, CardContent, CardMedia, CardActions, Typography, Grid, Skeleton, Pagination} from '@mui/material'
 
 export default function HistoryList() {
   const [history, setHistory] = useState([])
   const [loading, setLoading] = useState(false)
-  const [initialHistory, setInitialHistory] = useState()
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recordsPerPage] = useState(9);
+
+  const indexOfLastRecord = currentPage * recordsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+
+  const currentRecords = history.slice(indexOfFirstRecord, indexOfLastRecord);
+  const nPages = Math.ceil(history.length / recordsPerPage)
+  const pageNumbers = [...Array(nPages + 1).keys()].slice(1)
+
   const navigate = useNavigate()
   
   const loadHistory = async () => {
@@ -14,7 +24,6 @@ export default function HistoryList() {
     const response = await fetch('http://localhost:4000/history/')
     const data = await response.json()
     
-    setInitialHistory(data.length)
     setHistory(data)
     setLoading(false)
   }
@@ -35,21 +44,25 @@ export default function HistoryList() {
     }
   }
 
+  const handleChange = (event, value) => {
+    setCurrentPage(value)
+  }
+
   return (
     <>      
       <h1>History</h1>
       <Grid container spacing={2}>
       {        
-        (loading ? Array.from(new Array(12)) : history).map((history, index) => (
+        (loading ? Array.from(new Array(9)) : currentRecords).map((currentRecords, index) => (
           <Grid item xs={4} key={index}>  
             { 
-              history ?               
-              (<Card key={history.id} sx={{ marginBottom: ".7rem", maxWidth: 345, height: 700}}>
+              currentRecords ?               
+              (<Card key={currentRecords.id} sx={{ marginBottom: ".7rem", maxWidth: 345, height: 700}}>
                 <CardMedia
                   component="img"
-                  alt={history.titulo_original}
+                  alt={currentRecords.titulo_original}
                   height="500"
-                  image={history.img} 
+                  image={currentRecords.img} 
                 />
                 <CardContent style={{height: 110}}>
                   {/* TITULO */}
@@ -57,39 +70,39 @@ export default function HistoryList() {
                     gutterBottom variant="h5" 
                     component="div" 
                     title={
-                      history.titulo_original + (history.titulo_secundario ? ' | ' + history.titulo_secundario : '')
+                      currentRecords.titulo_original + (currentRecords.titulo_secundario ? ' | ' + currentRecords.titulo_secundario : '')
                     }>
-                    {String(history.titulo_original).length > 27 ? String(history.titulo_original).slice(0, 27) + '...' : history.titulo_original}
+                    {String(currentRecords.titulo_original).length > 27 ? String(currentRecords.titulo_original).slice(0, 27) + '...' : currentRecords.titulo_original}
                   </Typography>
-
+  
                   {/* DESCRIPCION     */}
                   <Typography 
                     variant="body2" 
                     color="text.secondary" 
                     title={
-                      String(history.descripcion).length > 200 ? history.descripcion : ''
+                      String(currentRecords.descripcion).length > 200 ? currentRecords.descripcion : ''
                   }>
-                    {String(history.descripcion).length > 200 ? String(history.descripcion).slice(0, 200) + '...' : history.descripcion}
+                    {String(currentRecords.descripcion).length > 200 ? String(currentRecords.descripcion).slice(0, 200) + '...' : currentRecords.descripcion}
                   </Typography>
                 </CardContent>
-
+  
                 {/* BOTONES EDIT/DELETE */}
                 <CardActions style={{marginTop: '.5rem'}}>
                   <Button variant='contained' 
                       color='inherit'                       
                       onClick={
-                        () =>{navigate(`/history/${history.id}/edit`)}
+                        () =>{navigate(`/history/${currentRecords.id}/edit`)}
                     }>
                       Edit
                     </Button>
-
+  
                     <Button variant='contained' 
                       color='warning'
                       style={{marginLeft: '12rem'}}
                       onClick={
                         () =>{
-                          if(window.confirm(`Eliminar ${history.titulo_original} del historial?`)){
-                            deleteHistory(history.id)
+                          if(window.confirm(`Eliminar ${currentRecords.titulo_original} del historial?`)){
+                            deleteHistory(currentRecords.id)
                           }                        
                         }
                     }>
@@ -103,20 +116,15 @@ export default function HistoryList() {
         ))
       }  
       </Grid>   
-      {console.log(history)}
-      {console.log(history[0])}
+
       {/* PAGINATION */}
-      <Grid container justifyContent="center">
-        <Stack spacing={2}>
-          <Pagination 
-            count={
-              initialHistory > 9 ? (Number.isInteger(initialHistory / 9) ? initialHistory / 9 : parseInt((initialHistory / 9) + 1) ) : 1
-            } 
-            page={1} 
-            variant="text" 
-            color="primary"/>          
-        </Stack>
+      <Grid container justifyContent="center">       
+        <Pagination count={pageNumbers.length} variant="outlined" page={currentPage} color='primary' shape="rounded" onChange={handleChange}/>
       </Grid>
     </>    
-  )   
+  )     
 }
+
+
+
+
